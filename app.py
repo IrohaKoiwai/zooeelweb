35fc3cbc1dc6d5d2e02900c9e4dc89c91ebfe0c7
mad_deer <380614677@qq.com>
2021-02-01 00:14:28 +0800

    created simple index page

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, escape, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'hello flask'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/blog')
+def blog_page():
+    return 'here is blog page'
+
+
+@app.route('/album')
+def album_page():
+    return 'here is album page'
+
+
+@app.route('/video')
+def video_page():
+    return 'here is video page'
+
+
+@app.route('/labo')
+def labo_page():
+    return 'here is laboratory page'
+
+
+@app.route('/about')
+def about_page():
+    return 'here is about page'
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
